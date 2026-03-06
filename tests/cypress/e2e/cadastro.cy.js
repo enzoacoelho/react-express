3c0db4e5a49393b51485eceac103908b97f2a3c2
enzoacoelho <enzoaugustocoelho@outlook.com>
2026-03-05 21:18:43 -0300

    alterando porta do frontend nos dockfiles e composer, adicionando primeiro teste cypress

---
+++ tests/cypress/e2e/cadastro.cy.js
@@ -0,0 +1,14 @@
+describe('Cadastro de Tutorial', () => {
+
+  beforeEach(() => {
+    cy.visit('/')
+  });
+
+  afterEach(() => {
+    cy.screenshot()
+  });
+
+  it('Deve fazer cadastro com sucesso', () => {
+   
+  })
+})
