7e8e50c5376a98bc3a6202f4034c40ff36741c99
enzoacoelho <enzoaugustocoelho@outlook.com>
2026-03-05 21:27:33 -0300

    primeiros testes

--- tests/cypress/e2e/cadastro.cy.js
+++ tests/cypress/e2e/cadastro.cy.js
@@ -1,7 +1,7 @@
 describe('Cadastro de Tutorial', () => {
 
   beforeEach(() => {
-    cy.visit('/')
+    cy.visit('http://localhost:8081/add')
   });
 
   afterEach(() => {
@@ -9,6 +9,13 @@ describe('Cadastro de Tutorial', () => {
   });
 
   it('Deve fazer cadastro com sucesso', () => {
-   
+   cy.get(':nth-child(2) > .nav-link').click()
+
+   cy.get('[name="title"]').type('Testando Titulo')
+   cy.get(':nth-child(2) > [name="description"]').type('Testando descricao')
+
+   cy.get('.btn').click()
+
+   cy.get('h4').should('contain', 'You submitted successfully!')
   })
 })
